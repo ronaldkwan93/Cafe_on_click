88ef3b26ec5ae0be905186c05ed9a62def65e582
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-05-09 23:19:29 +1000

    added product modal

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,49 @@
-import classes from './ProductCard.module.scss'
+import { useState, useEffect } from "react";
+import ProductModal from "../ProductModal/ProductModal";
+import classes from "./ProductCard.module.scss";
 
 const ProductCard = () => {
+  const [modalStatus, setModalStatus] = useState(false);
+
+  const showModal = () => {
+    setModalStatus(true);
+  };
+
+  const closeModal = () => {
+    setModalStatus(false);
+  };
+
+  useEffect(() => {
+    if (modalStatus) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [modalStatus]);
+
   return (
-    <div className={classes.card}>
+    <>
+      <div className={classes.card} onClick={showModal}>
         <img src="https://placecats.com/neo_banana/250/200" alt="" />
         <p className={classes.card__plus}>+</p>
         <div className={classes.card__text}>
-            <h4>Title</h4>
-            <p>(1000kJ)</p>
-            <h4>A$10.00</h4>
+          <h4>Title</h4>
+          <p>(1000kJ)</p>
+          <h4>A$10.00</h4>
         </div>
+      </div>
+      {modalStatus && (
+        <ProductModal closeModal={closeModal}>
+           
+         
+        </ProductModal>
+      )}
+    </>
+  );
+};
 
-    </div>
-  )
-}
-
-export default ProductCard
+export default ProductCard;
---
+++ src/components/ProductModal/ProductModal.jsx
@@ -0,0 +1,30 @@
+import classes from "./ProductModal.module.scss";
+
+const ProductModal = ({ closeModal }) => {
+  const handleContainerClick = (e) => {
+    e.stopPropagation();
+  };
+  return (
+    <div className={classes.backdrop} onClick={closeModal}>
+      <div className={classes.container} onClick={handleContainerClick}>
+        <p onClick={closeModal} className={classes.container__exit}>
+          X
+        </p>
+        <h1>Title</h1>
+        <p>(1000kJ)</p>
+        <h4>A$10.00</h4>
+        <img src="https://placecats.com/neo_banana/600/400" alt="" />
+        <div className={classes.container__addCart}>
+          <div className={classes.container__addCart__count}>
+            <button>-</button>
+            <p>count</p>
+            <button>+</button>
+          </div>
+          <button className={classes.container__addCart_btn}>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductModal;
