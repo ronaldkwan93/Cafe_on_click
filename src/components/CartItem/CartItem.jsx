7beaf0c201adb4c262f6f7823c8bd0fb975349ab
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-05-10 16:39:53 +1000

    add cart modal quantity change

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,20 +2,30 @@ import { useState } from 'react';
 import classes from './CartItem.module.scss'
 
 const CartItem = ({item, handleRemove}) => {
-    const [quantity, setQuantity] = useState(item.quantity)
+    const [quantity, setQuantity] = useState(item.quantity);
+    const totalPrice = quantity * item.price;
+
+    const handleDecrement = () => {
+        setQuantity(quantity - 1)
+    }
+
+    const handleIncrement = () => {
+        setQuantity(quantity + 1)
+    }
+
   return (
     <div className={classes.item}>
       <img src={item.imgUrl} alt={item.title} />
       <div>
         <p>{item.title}</p>
         <p>{item.kJ}kJ</p>
-        <p>A${item.price?.toFixed(2) ?? ""}</p>
+        <p>A${totalPrice}</p>
         
       </div>
       <div className={classes.item__counter}>
-        <button>-</button>
+        <button onClick={handleDecrement} disabled={quantity <= 1}>-</button>
         <p>{quantity}</p>
-        <button>+</button>
+        <button onClick={handleIncrement}>+</button>
       </div>
       <div onClick={() => handleRemove(item)}>Delete</div>
     </div>
